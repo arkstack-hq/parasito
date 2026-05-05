068b4063c5c9dad805acf295e0d1b291d59c3a20
3m1n3nc3 <mygames.ng@gmail.com>
2026-05-05 04:21:59 +0100

    refactor: update tsdown configuration to include clean and sourcemap options

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'tsdown'
 
 export default defineConfig({
+    clean: true,
     format: ['esm', 'cjs'],
     minify: true,
     exports: true,
+    sourcemap: false,
     outputOptions: {
         exports: 'named',
     },
