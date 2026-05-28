63e46eacfc257486358991809f0ac7099ee5c4cb
exronnin <neweminence@gmail.com>
2026-05-28 18:56:27 +0100

    chore: set VERBOSITY env to 0

--- vitest.config.ts
+++ vitest.config.ts
@@ -17,6 +17,7 @@ export default defineConfig({
         ],
         env: {
             NODE_ENV: 'test',
+            VERBOSITY: '0'
         },
         coverage: {
             reporter: ['text', 'json', 'html', 'lcov'],
